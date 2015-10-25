a17d3c59c1f03a0235bba4ba0b68b58790f88dfc
stevensnippe <steven.snippe@student.hu.nl>
2015-10-25 20:02:48 +0100

    Update Thuisbioscoop.py

--- Thuisbioscoop.py
+++ Thuisbioscoop.py
@@ -3,7 +3,7 @@ import time
 import tkinter
 
 """
-* master branch password: v1q
+* master branch password kan je vinden op WhatsApp
 Aan de parameter key geeft je de eigen unieke API sleutel mee.
 Aan de parameter datum geeft je de datum mee geschreven als: 25-10-2015. Je kunt alleen een request doen naar de TV-films voor vandaag en morgen.
 Aan de parameter sorteer geeft je mee welke films je wilt ophalen. Dit is het getal 0, 1 of 2. Waar 0 staat voor: alle films, 1 staat voor: filmtips, 2 staat voor: 'film van de dag'.
