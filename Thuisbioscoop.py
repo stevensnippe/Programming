0da673909262cd81cc5ca8a8203d260a35134869
stevensnippe <steven.snippe@student.hu.nl>
2015-10-26 01:34:31 +0100

    10/26/2015 - 1:32

--- Thuisbioscoop.py
+++ Thuisbioscoop.py
@@ -1,8 +1,10 @@
 import requests #get data from API
 import time     #get current date
 import tkinter  #draw interface
+import codecs
+import xmltodict
 
-# ~---> master branch password kan je vinden op WhatsApp <---~
+# ~---> master branch password is v1q <---~
 """
 Aan de parameter key geeft je de eigen unieke API sleutel mee.
 Aan de parameter datum geeft je de datum mee geschreven als: 25-10-2015. Je kunt alleen een request doen naar de TV-films voor vandaag en morgen.
@@ -13,4 +15,26 @@ datum = time.strftime("%d-%m-%Y")
 sorteer = 0
 response = requests.get('http://www.filmtotaal.nl/api/filmsoptv.xml?apikey='+key+'&dag='+datum+'&sorteer='+str(sorteer))
 #print(response.text)
-print("test")
+
+def schrijf_xml(reponse):
+    bestand = codecs.open('films.xml', "w", "utf-8")
+    bestand.write(str(response.text))
+    bestand.close()
+
+def verwerk_xml():
+    bestand = open('films.xml', 'r')
+    xml_string = bestand.read()
+    bestand.close()
+    return xmltodict.parse(xml_string)
+
+def print_filmnamen(film_dict):
+    for film in film_dict['filmsoptv']['film']:
+        print(film['titel']+" "+str(film['zender']))
+        #print('{} {}'.format(film['titel'], str(film['zender'])))
+        #print("Titel: "+film['titel']+" Zender:"+str(film['zender']))
+
+schrijf_xml(response)
+films_dict = verwerk_xml()
+print_filmnamen(films_dict)
+
+
