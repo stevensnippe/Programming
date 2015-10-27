732b9c94a958ad3dd61ae4f141235d3232782e72
Mitchell van Ek <a.sunnyband@gmail.com>
2015-10-27 10:36:00 +0100

    27/10/2015    10:35     Mitchell patch_2: login screen gemaakt (moet nog netjes worden)

--- Tkinter.py
+++ Tkinter.py
@@ -1,52 +1,93 @@
 import tkinter
 # from PIL import ImageTk, Image
 
+tk = tkinter
 
-def getText(text, password):
+
+def gettext(text, password):
     from tkinter.messagebox import showinfo
     showinfo(title='popup', message='Hoi '+text)
 
 
 def newuser():
+    global newuserwindow
     window.destroy()
+    rommel.destroy()
     newuserwindow = tkinter.Tk()
     newuserwindow.title("Flexchill")
-    newuserwindow.wm_iconbitmap("favicon.ico")  #de logo van het programma
+    newuserwindow.wm_iconbitmap("favicon.ico")  # de logo van het programma
     newuserwindow.configure(background="black")
 
+    lname = tk.Label(newuserwindow, text="Name:", fg="white", bg="black")
+    ename = tk.Entry(newuserwindow)
+
+    lemail = tk.Label(newuserwindow, text="Email:", fg="white", bg="black")
+    eemail = tk.Entry(newuserwindow)
+
+    lpassword = tk.Label(newuserwindow, text="Password:", fg="white", bg="black")
+    epassword = tk.Entry(newuserwindow)
+
+    makeaccount = tk.Button(newuserwindow, text="Make Account")
+    gobackwindow = tk.Button(newuserwindow, text="Back", command=(lambda: goback(1)))  # werkt niet, geeft errors (zie functie goback(a))
+
+    lname.pack()
+    ename.pack()
+    lemail.pack()
+    eemail.pack()
+    lpassword.pack()
+    epassword.pack()
+    makeaccount.pack()
+    gobackwindow.pack(side="left")
     newuserwindow.mainloop()
 
 
-window = tkinter.Tk()
-window.geometry("300x300")
-window.title("Flexchill")
-window.wm_iconbitmap("favicon.ico")  #de logo van het programma
-window.configure(background="black")
-
-tk = tkinter
-
-photo = tk.PhotoImage(file="deze.gif") #werkt nog niet, weet niet waarom
-w = tk.Label(window, image=photo)
-# w.place(x=0, y=0, relwidth=1, relheight=1) #voor het geval je het als achtergrond wil plaatsen, w.pack() moet uit staan
+def goback(a):
+    if a == 1:
+        global newuserwindow
+        newuserwindow.destroy()
+        menu()
 
 
-label = tk.Label(window, text='Email:', fg="white", bg="black")
+def menu():
+    """
+   Let op! programma sluit pas echt af als je op quit drukt"""
+    global window
+    global rommel
+    rommel = tkinter.Tk()  # houd een tweede scherm tegen
+    rommel.withdraw()
+    window = tkinter.Toplevel()
+    window.geometry("300x300")
+    window.title("Flexchill")
+    window.wm_iconbitmap("favicon.ico")  # de logo van het programma
+    window.configure(background="black")
 
-email = tk.Entry(window)
+    photo = tk.PhotoImage(file="deze.gif")
+    w = tk.Label(window, image=photo)
+    w.image = photo
+    # w.place(x=0, y=0, relwidth=1, relheight=1) #voor het geval je het als achtergrond wil plaatsen, w.pack() moet uit staan
 
-passlabel = tk.Label(window, text="Password:", fg="white", bg="black")
+    label = tk.Label(window, text='Username:', fg="white", bg="black")
 
-password = tk.Entry(window)
+    email = tk.Entry(window)
 
-bsignin = tk.Button(window, text='Sign in',command=(lambda: getText(email.get(), password.get())))
+    passlabel = tk.Label(window, text="Password:", fg="white", bg="black")
 
-bsignup = tk.Button(window, text="Sign up", command=(lambda: newuser()))
-#hieronder staat de volgorde van de programma's
-w.pack(side="top")
-label.pack()
-email.pack()
-passlabel.pack()
-password.pack()
-bsignin.pack()
-bsignup.pack(side="right")
-window.mainloop()
+    password = tk.Entry(window)
+
+    bsignin = tk.Button(window, text='Sign in', command=(lambda: gettext(email.get(), password.get())))
+
+    bsignup = tk.Button(window, text="Sign up", command=(lambda: newuser()))
+
+    bquit = tk.Button(window, text="Quit", command=(lambda: rommel.destroy() and window.destroy()))
+    # hieronder staat de volgorde van de programma's
+    w.pack(side="top")
+    label.pack()
+    email.pack()
+    passlabel.pack()
+    password.pack()
+    bsignin.pack()
+    bsignup.pack(side="right")
+    bquit.pack(side="bottom")
+    window.mainloop()
+
+menu()
