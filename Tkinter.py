2aafe8972a231fa7993f6685cbcad6d803ab0971
stevensnippe <steven.snippe@student.hu.nl>
2015-10-27 03:39:44 +0100

    10/27/2015 - 3:39AM || Steven || login redelijk klaar, nieuwe interfaces nodig

--- Tkinter.py
+++ Tkinter.py
@@ -10,9 +10,19 @@ def logIn():
         pw = password.get()
         accesGranted = TB.login(user, pw)
         #print(accesGranted)
-        if accesGranted == True:
-            print("ja")
-            #hier de code om naar het volgende scherm te gaan waar films worden gedisplayed
+        if accesGranted == True: #TODO: image en tekst naar het midden bij succesful login
+            print("test")
+            username.pack_forget() #hide field
+            password.pack_forget()
+            bsignin.pack_forget()
+            attemptsLeft.pack_forget()
+            label.pack_forget()
+            passlabel.pack_forget()
+            bsignup.pack_forget()
+            warning.pack_forget()
+            label2 = tk.Label(window, text='Login succesful, u word doorverwezen.', fg="white", bg="black")
+            label2.pack()
+            #TODO: hier de code om naar het volgende scherm te gaan waar films worden gedisplayed (nieuwe def)
             return True
         else:
             TB.loginPogingen -= 1
@@ -22,10 +32,10 @@ def logIn():
                 warning['text'] = "Too many failed login attempts - wait 5 minutes."
                 #username['bg'] = "red"
                 #password['bg'] = "red"
-                username.configure(state = "disabled")
-                password.configure(state = "disabled")
-                bsignin.configure(state = "disabled")
-                photo.configure(file="blocked.gif")
+                username.configure(state="disabled")
+                password.configure(state="disabled")
+                bsignin.configure(state="disabled")
+                photo.configure(file="blocked.gif") #kan wel beter dan dit maar het idee is er
             return False
     else:
         warning['text'] = "Too many failed login attempts - wait 5 minutes."
@@ -49,7 +59,7 @@ window.configure(background="black")
 
 tk = tkinter
 
-photo = tk.PhotoImage(file="deze.gif") #werkt nog niet, weet niet waarom
+photo = tk.PhotoImage(file="deze.gif") #werkt nog niet, weet niet waarom - werkt wel
 w = tk.Label(window, image=photo)
 # w.place(x=0, y=0, relwidth=1, relheight=1) #voor het geval je het als achtergrond wil plaatsen, w.pack() moet uit staan
 
@@ -60,7 +70,7 @@ username = tk.Entry(window, bg="white")
 
 passlabel = tk.Label(window, text="Password:", fg="white", bg="black")
 
-password = tk.Entry(window, bg="white")
+password = tk.Entry(window, bg="white", show="*")
 
 bsignin = tk.Button(window, text='Sign in',command=(lambda: logIn()))
 
