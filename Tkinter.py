2c1ec68cc5d9f7828cec2ad1311865fbe924d0b8
stevensnippe <steven.snippe@student.hu.nl>
2015-10-27 02:30:44 +0100

    10/27/2015 - 2:29AM || Steven || import Thuisbioscoop

--- Tkinter.py
+++ Tkinter.py
@@ -1,10 +1,25 @@
 import tkinter
+import Thuisbioscoop as TB
 # from PIL import ImageTk, Image
 
 
-def getText(text, password):
-    from tkinter.messagebox import showinfo
-    showinfo(title='popup', message='Hoi '+text)
+
+def logIn():
+    user = username.get()
+    pw = password.get()
+    accesGranted = TB.login(user, pw)
+    print(accesGranted)
+    if accesGranted == True:
+        print("ja")
+        #hier de code om naar het volgende scherm te gaan waar films worden gedisplayed
+        return True
+    else:
+        TB.loginPogingen -= 1
+        attemptsLeft['text'] = "Attempts left: "+str(TB.loginPogingen)
+        warning['text'] = "Login failed"
+        return False
+
+
 
 
 def newuser():
@@ -13,7 +28,6 @@ def newuser():
     newuserwindow.title("Flexchill")
     newuserwindow.wm_iconbitmap("favicon.ico")  #de logo van het programma
     newuserwindow.configure(background="black")
-
     newuserwindow.mainloop()
 
 
@@ -30,23 +44,29 @@ w = tk.Label(window, image=photo)
 # w.place(x=0, y=0, relwidth=1, relheight=1) #voor het geval je het als achtergrond wil plaatsen, w.pack() moet uit staan
 
 
-label = tk.Label(window, text='Email:', fg="white", bg="black")
+label = tk.Label(window, text='Username:', fg="white", bg="black")
 
-email = tk.Entry(window)
+username = tk.Entry(window)
 
 passlabel = tk.Label(window, text="Password:", fg="white", bg="black")
 
 password = tk.Entry(window)
 
-bsignin = tk.Button(window, text='Sign in',command=(lambda: getText(email.get(), password.get())))
+bsignin = tk.Button(window, text='Sign in',command=(lambda: logIn()))
+
+attemptsLeft = tk.Label(window, text="Attempts left: 3", fg="white", bg="black")
+
+warning = tk.Label(window, text="", fg="red", bg="black")
 
 bsignup = tk.Button(window, text="Sign up", command=(lambda: newuser()))
 #hieronder staat de volgorde van de programma's
 w.pack(side="top")
 label.pack()
-email.pack()
+username.pack()
 passlabel.pack()
 password.pack()
 bsignin.pack()
+attemptsLeft.pack()
+warning.pack()
 bsignup.pack(side="right")
 window.mainloop()
