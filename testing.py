7f2fe4296da5e01e9fcd6846a5535b077ed9764a
Mitchell van Ek <a.sunnyband@gmail.com>
2015-10-30 11:59:07 +0100

    29/10/2015    11:29     Mitchell

--- testing.py
+++ testing.py
@@ -3,15 +3,19 @@ TB.schrijf_xml(TB.response)
 TB.films_dict = TB.verwerk_xml()
 global filmnamen
 filmnamen = TB.print_filmnamen(TB.films_dict)
-# provider = filmnamen["provider"].index("RTL8")
-# print(provider)
+naam = "RTL8"
 list = []
+filmnamenlijst = []
 nummer = 0
 for i in filmnamen["provider"]:
-    if i == "RTL8":
+    if i == naam:
         print(nummer)
         list.append(nummer)
     nummer += 1
 for provider in list:
     print(filmnamen["titel"][provider])
+    filmnamenlijst.append(filmnamen["titel"][provider])
 print(filmnamen["provider"])
+
+return filmnamenlijst
+
