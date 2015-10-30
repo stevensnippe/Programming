7b03d47e3d8a12b76c1fffff54bed131e16eaa7c
stevensnippe <steven.snippe@student.hu.nl>
2015-10-30 16:05:00 +0100

    Relevant voor leraar

--- Tkinter.py
+++ Tkinter.py
@@ -41,7 +41,7 @@ def filmscreen():
     TB.schrijf_xml(TB.response)
     TB.films_dict = TB.verwerk_xml()
     global filmnamen
-    filmnamen = TB.print_filmnamen(TB.films_dict)  # filmnamen geeft alle huidige films in list
+    filmnamen = TB.print_filmnamen(TB.films_dict)  # filmnamen geeft alle huidige films weer in een list
     rij = 0
 
     for i in filmnamen["titel"]:  # http://stackoverflow.com/questions/7300041/tkinter-create-labels-and-entrys-dynamically
